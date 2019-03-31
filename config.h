8ab35f6a558325adacdab21e400db16079d7ed7b
Jeff Cooke <24613307+DrJeffCooke@users.noreply.github.com>
2019-03-31 10:24:37 -0500

    Further debugging Spark EV battery module data
    1) Further debugging on the Spark 2014 EV battery module (BCM) communications and control
    2) Switched back to relays for Battery Pack contactors control
    3) Debugged CAN communications with the TesLorean CAN network, TesLCAN controls the BCM via CAN
    4) Updated the wiring diagram for the controller
    Still pending updates
    - BCM confirmation CAN frames after receiving instructions
    - More testing with the Pack Halves temperature readings (need calibrating)

--- config.h
+++ config.h
@@ -2,16 +2,16 @@
 #define __CONFIG_H__
 
 // Cell Voltage warning levels, unit 1/100th Volt
-#define CELL_LOW_LOW_VOLTAGE 280
-#define CELL_LOW_VOLTAGE 300
-#define CELL_HIGH_VOLTAGE 345 
-#define CELL_HIGH_HIGH_VOLTAGE 365
+#define CELL_LOW_LOW_VOLTAGE 250
+#define CELL_LOW_VOLTAGE 260
+#define CELL_HIGH_VOLTAGE 350 
+#define CELL_HIGH_HIGH_VOLTAGE 360
 
 // Pack Voltage warning levels, unit volts
-#define PACK_LOW_LOW_VOLTAGE 314
-#define PACK_LOW_VOLTAGE 320
-#define PACK_HIGH_VOLTAGE  400
-#define PACK_HIGH_HIGH_VOLTAGE 408
+#define PACK_LOW_LOW_VOLTAGE 280
+#define PACK_LOW_VOLTAGE 292
+#define PACK_HIGH_VOLTAGE  393
+#define PACK_HIGH_HIGH_VOLTAGE 403
 
 // Battery warning temps
 #define PACK_LOW_LOW_TEMP 40
@@ -23,11 +23,17 @@
 #define RELAY_ON 0
 #define RELAY_OFF 1
 
+// CAN board Chip Select (CS) pins
+#define TESLOREAN_CAN_CS 9
+#define TESLOREAN_CAN_INT 3
+#define BATTERY_CAN_CS 10
+#define BATTERY_CAN_INT 2
+
 // Relay Connections - Connector X358 (contactors control)
-#define Relay_X358_7  3  // X358-7 K3: Main -VE [G]
-#define Relay_X358_8  4  // X358-8 Multifunction contactor [E]
-#define Relay_X358_9  5  // X358-9 K1: Precharge contactor for Main [L]
-#define Relay_X358_10  6  // X358-10 K2: Main +VE contactor [F]
+#define Relay_X358_10  12  // X358-10 K2: Main +VE contactor [F]
+#define Relay_X358_7  4  // X358-7 K3: Main -VE [G]
+#define Relay_X358_8  5  // X358-8 Multifunction contactor [E]
+#define Relay_X358_9  6  // X358-9 K1: Precharge contactor for Main [L]
 
 // Relay Connections - Connector X357 (data connection)
 #define Relay_X357_4  7  // X357-3 to 12v accessory wake up
@@ -48,7 +54,7 @@
 //********* DELAY SETTINGS ******************
 #define DELAY_PRECHARGE 2000    // Max time (in ms) that the Pre-charger contactor is closed, and +VE open
 #define DELAY_INTERRELAY 500    // Time between the contactor actions
-#define DELAY_STATUSMSG 500    // Time between battery status messages
+#define DELAY_STATUSMSG 2000    // Time between battery status messages
 #define DELAY_MODFMSG 10000    // Time between battery module temp messages
 #define DELAY_PCKFMSG 30000    // Time between battery pack temp messages
 
